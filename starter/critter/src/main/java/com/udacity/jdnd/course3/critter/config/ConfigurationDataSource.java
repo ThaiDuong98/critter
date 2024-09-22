3e9ded6fd5a11ff2e619d5e3cf67e9fbb82a2f81
duongpham <thaiduong6898@gmail.com>
2024-09-22 08:44:41 +0700

    update config data source file

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/config/ConfigurationDataSource.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/config/ConfigurationDataSource.java
@@ -4,16 +4,28 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class ConfigurationDataSource {
     @Bean
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource getDataSource(){
-        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.url("jdbc:mysql://localhost:3306/critter?useSSL=false&serverTimezone=UTC");
-        return dataSourceBuilder.build();
+    @Profile("test")
+    public DataSource getTestDataSource() {
+        return DataSourceBuilder.create()
+                .url("jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE")
+                .driverClassName("org.h2.Driver")
+                .username("sa")
+                .password("")
+                .build();
     }
+
+//    @Bean
+//    @ConfigurationProperties(prefix = "spring.datasource")
+//    public DataSource getDataSource(){
+//        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+//        dataSourceBuilder.url("jdbc:mysql://localhost:3306/critter?useSSL=false&serverTimezone=UTC");
+//        return dataSourceBuilder.build();
+//    }
 }
